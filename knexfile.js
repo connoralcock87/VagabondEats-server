ee67abe853ba86dfc81729c8361f5fe17ca4a86d
Connor <connor.alcock.87@gmail.com>
2017-10-24 10:26:26 -0600

    Express server and MySQL database created

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+     host: '127.0.0.1',
+     user: 'root',
+     password: 'password',
+     database: '<foodTruck_db>',
+     charset: 'utf8'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+    }
+};
